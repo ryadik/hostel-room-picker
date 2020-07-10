1ea7bfdbcb880b1104f0945856fa1344e409817a
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-11 01:43:47 +0800

    make list-buttons block IN list and make apply button

---
+++ src/pages/includes/UI-kit/pages/dropdown/list/list-button/__apply/apply.js
@@ -0,0 +1,4 @@
+document.querySelector('.list-button__apply').addEventListener('click', () => {
+  document.querySelector('.dropdown').classList.remove('dropdown_active')
+  document.querySelector('.list').classList.remove('list_active')
+})
