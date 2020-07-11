d24916c0f79e0dfbfbc411b836d68859fe47dcb7
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-11 18:27:08 +0800

    added competency to selection element

--- src/pages/includes/UI-kit/pages/dropdown/__selection/selection.js
+++ src/pages/includes/UI-kit/pages/dropdown/__selection/selection.js
@@ -1,21 +1,29 @@
-const btnsDecrement = document.querySelectorAll('.option-button__decrement')
-const btnsIncrement = document.querySelectorAll('.option-button__increment')
-const selectionText = document.querySelector('.dropdown__selection')
+const btnsDecrement = document.querySelectorAll('.option-button__decrement'),
+      btnsIncrement = document.querySelectorAll('.option-button__increment'),
+      selectionText = document.querySelectorAll('.dropdown__selection')
 
 // menu (List) - open and close
-document.querySelector('.dropdown__selection').addEventListener('click', function () {
-  document.querySelector('.dropdown').classList.toggle('dropdown_active')
-  document.querySelector('.list').classList.toggle('list_active')
-})
-
-for (let i = 0; i < btnsDecrement.length; i++) {
-  btnsDecrement[i].addEventListener('click', () => {
-    setTimeout(() => {
-      selectionText.innerHTML = localStorage.getItem('selectionText')
-    }, 100)
-  })
-  clearTimeout()
+for (let i = 0; i < selectionText.length; i++) {
+  btnOpenId = selectionText[i].data
+  if (selectionText[i].dataset.id == (i + 1)) {
+    selectionText[i].addEventListener('click', function() {
+      document.querySelector(`div[data-id="${i + 1}"]`).classList.toggle('dropdown_active')
+      document.querySelector(`div[data-id="${i + 1}"] .list`).classList.toggle('list_active')
+    })
+  }
 }
+
+
+// for (let i = 0; i < btnsDecrement.length; i++) {
+//   btnsDecrement[i].addEventListener('click', () => {
+//     setTimeout(() => {
+//       selectionText.innerHTML = localStorage.getItem('selectionText')
+//     }, 100)
+//   })
+//   clearTimeout()
+// }
+
+// for (let i = 0; )
 
 for (let i = 0; i < btnsIncrement.length; i++) {
   btnsIncrement[i].addEventListener('click', () => {
@@ -24,11 +32,6 @@ for (let i = 0; i < btnsIncrement.length; i++) {
     }, 100)
   })
   clearTimeout()
-}
-
-function setSelectionText() {
-  
-  // clearInterval()
 }
 
 
