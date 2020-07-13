585037fc8f376f81268daa21ced3271866984bde
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-14 05:02:16 +0800

    added localization to calendar

--- src/pages/includes/UI-kit/pages/calendar/calendar.js
+++ src/pages/includes/UI-kit/pages/calendar/calendar.js
@@ -1,8 +1,14 @@
-import 'dhx-calendar'
+import * as dhx from 'dhx-calendar'
 
-calendarContainer = document.querySelector('#calendar_container')
-
-const calendar = new dhx.Calendar(calendarContainer,{
-  range: true,
-  css: "dhx_widget--bordered"
-});
+let ru = {
+  // short names of months
+  monthsShort: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сен", "Окт", "Нояб", "Дек"],
+  // full names of months             
+  months: ["Январь", "Февраль", "Март", "Апрель", "Мая", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+  // short names of days
+  daysShort: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+  // full names of days
+  days: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскреснье"]
+}
+dhx.i18n.setLocale("calendar", ru);
+let calendar = new dhx.Calendar("calendar", {css: "dhx_widget--bordered", range: true, width: '320px'});
