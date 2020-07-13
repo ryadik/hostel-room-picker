6e162affc86d3da3c4e5de7ececd4c196576d8a2
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-14 02:44:54 +0800

    absolutly new script for clean button

--- src/pages/includes/UI-kit/pages/dropdown/list/list-button/__clean/clean.js
+++ src/pages/includes/UI-kit/pages/dropdown/list/list-button/__clean/clean.js
@@ -1,42 +1,19 @@
-const btnClean = document.querySelector('.list-button__clean')
-const btnsDecrement = document.querySelectorAll('.option-button__decrement')
-const btnsIncrement = document.querySelectorAll('.option-button__increment')
-const counters = document.querySelectorAll('.option-button__counter')
-const selectionText = document.querySelector('.dropdown__selection')
+const drop = document.querySelectorAll('.dropdown') // all dropdown on page
 
-for (let i = 0; i < btnsDecrement.length; i++) {
-  btnsDecrement[i].addEventListener('click', () => {
-    setTimeout(() => {
-      if (localStorage.getItem('sumCountersValue') == 0) {
-        btnClean.classList.remove('list-button__clean_active')
-      }
-    }, 100)
+for (let i = 0; i < drop.length; i++) {
+  let btnClean = document.querySelector(`div[data-id="${i + 1}"] .list-button__clean`),
+      cntrs = document.querySelectorAll(`div[data-id="${i + 1}"] .option-button__counter`), // counters in current dropdown
+      selectionField = document.querySelector(`div[data-id="${i + 1}"] .dropdown__selection`)
+  console.log(cntrs)
+  btnClean.addEventListener('click', () => {
+    deleteCntrsValue(cntrs)
+    btnClean.classList.remove('list-button__clean_active')
+    selectionField.value = ''
   })
-  clearTimeout()
-}
-for (let i = 0; i < btnsIncrement.length; i++) {
-  btnsIncrement[i].addEventListener('click', () => {
-    setTimeout(() => {
-      if (localStorage.getItem('sumCountersValue') > 0) {
-        btnClean.classList.add('list-button__clean_active')
-      }
-    }, 100)
-  })
-  clearTimeout()
 }
 
-btnClean.addEventListener('click', () => {
-  for (let i = 0; i < counters.length; i++) {
-    counters[i].innerHTML = 0
+function deleteCntrsValue (cntrs) {
+  for (cnt of cntrs) {
+    cnt.innerHTML = 0
   }
-  removeActive()
-  localStorage.clear()
-  selectionText.innerHTML = 'Сколько гостей'
-})
-
-function removeActive () {
-  for (let i = 0; i < btnsDecrement.length; i++) {
-    btnsDecrement[i].classList.remove('option-button__decrement_active')
-  }
-  btnClean.classList.remove('list-button__clean_active')
 }
