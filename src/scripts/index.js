012715330c255ad691d78621debd8894ec750c01
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-14 18:33:22 +0800

    added font-awesome to page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,9 @@
 import * as $ from 'jquery'
+import dhx from 'dhx-calendar'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 
 import '@/styles/main.sass'
