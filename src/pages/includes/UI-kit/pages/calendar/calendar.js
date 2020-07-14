7804c4bee5209568d4a19f99f1e3c61d14c329ca
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-14 22:08:07 +0800

    change nav-buttons in calendar plugin

--- src/pages/includes/UI-kit/pages/calendar/calendar.js
+++ src/pages/includes/UI-kit/pages/calendar/calendar.js
@@ -1,5 +1,6 @@
 import * as dhx from 'dhx-calendar'
 
+
 let ru = {
   // short names of months
   monthsShort: ["Янв", "Фев", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сен", "Окт", "Нояб", "Дек"],
@@ -12,3 +13,7 @@ let ru = {
 }
 dhx.i18n.setLocale("calendar", ru);
 let calendar = new dhx.Calendar("calendar", {css: "dhx_widget--bordered", range: true, width: '320px'});
+
+// $('.dxi-chevron-left').addClass('fas fa-arrow-left')
+document.querySelector('.dhx_calendar-action__prev').innerHTML ="<span class='icon-arrow_back'></span>"
+document.querySelector('.dhx_calendar-action__next').innerHTML ="<span class='icon-arrow_forward'></span>"
