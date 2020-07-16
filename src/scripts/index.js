95e241efa4db3810e486d3d480f85b182a081420
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-17 04:02:23 +0800

    deletes useless code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,3 @@
-import * as $ from 'jquery'
-import dhx from 'dhx-calendar'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
-
 import '@/styles/main.sass'
-import '@/scripts/babel.js'
+// import '@/scripts/babel.js'
 
