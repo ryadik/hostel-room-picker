f7c71f1bf9addc9a419686f6ff2b9ac614dd7631
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-18 22:58:57 +0800

    initialization filter-date-dropdown.js in datepicker.js

--- src/pages/includes/UI-kit/pages/datepicker/datepicker.js
+++ src/pages/includes/UI-kit/pages/datepicker/datepicker.js
@@ -4,3 +4,4 @@ $.fn.datepicker.language.ru.today = 'применить';
 
 require('../masked-textfield/masked-textfield.js');
 require('../date-dropdown/date-dropdown.js');
+require('../filter-date-dropdown/filter-date-dropdown.js');
