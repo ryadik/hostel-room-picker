9ceed58bdd873dadede30e48751f1892dc41d776
K1nGsmaN-hub <15162342h@gmail.com>
2020-07-18 22:45:49 +0800

    removed using calendar plugin from datepicker.js

--- src/pages/includes/UI-kit/pages/datepicker/datepicker.js
+++ src/pages/includes/UI-kit/pages/datepicker/datepicker.js
@@ -1,28 +1,6 @@
-import 'air-datepicker'
+import 'air-datepicker';
 
-$.fn.datepicker.language['ru']['today'] = 'применить'
+$.fn.datepicker.language.ru.today = 'применить';
 
-$('.calendar').datepicker({
-  clearButton: true, // clear button
-  todayButton: true,
-
-  //nav buttons icons
-  prevHtml: '<span class="icon-arrow_back"></span>',
-  nextHtml: '<span class="icon-arrow_forward"></span>',
-
-  autoClose: true,
-
-  // titles text
-  navTitles: {
-    days: ' MM yyyy'
-  },
-})
-
-const btnsApply = [...document.querySelectorAll('span[data-action="today"]')]
-
-btnsApply.forEach((elem, index) => {
-  elem.addEventListener('click', () => {
-    let datepicker = $(`.calendar[data-id=${index}]`).datepicker().data('datepicker')
-    datepicker.hide()
-  })
-})
+require('../masked-textfield/masked-textfield.js');
+require('../date-dropdown/date-dropdown.js');
