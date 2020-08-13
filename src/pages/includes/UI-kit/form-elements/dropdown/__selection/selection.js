e420284c2383439976d6119ee86a1b0e0e3fbffc
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-13 19:24:46 +0800

    refactoring selection.js in dropdown

--- src/pages/includes/UI-kit/form-elements/dropdown/__selection/selection.js
+++ src/pages/includes/UI-kit/form-elements/dropdown/__selection/selection.js
@@ -1,34 +1,11 @@
-const btnsDecrement = document.querySelectorAll('.option-button__decrement');
-const btnsIncrement = document.querySelectorAll('.option-button__increment');
 const selectionText = document.querySelectorAll('.dropdown__selection');
 
 // menu (List) - open and close
-for (let i = 0; i < selectionText.length; i++) {
-  btnOpenId = selectionText[i].data;
-  if (selectionText[i].dataset.id == (i + 1)) {
-    selectionText[i].addEventListener('click', () => {
-      document.querySelector(`div[data-id="${i + 1}"]`).classList.toggle('dropdown_active');
-      document.querySelector(`div[data-id="${i + 1}"] .list`).classList.toggle('list_active');
-    });
-  }
-}
 
-// for (let i = 0; i < btnsDecrement.length; i++) {
-//   btnsDecrement[i].addEventListener('click', () => {
-//     setTimeout(() => {
-//       selectionText.innerHTML = localStorage.getItem('selectionText')
-//     }, 100)
-//   })
-//   clearTimeout()
-// }
+selectionText.forEach((btn, index) => {
+  btn.addEventListener('click', (e) => {
+    e.target.offsetParent.classList.toggle('dropdown_active')
+    e.target.offsetParent.lastChild.classList.toggle('list_active')
+  })
+})
 
-// for (let i = 0; )
-
-for (let i = 0; i < btnsIncrement.length; i++) {
-  btnsIncrement[i].addEventListener('click', () => {
-    setTimeout(() => {
-      selectionText.innerHTML = localStorage.getItem('selectionText');
-    }, 100);
-  });
-  clearTimeout();
-}
