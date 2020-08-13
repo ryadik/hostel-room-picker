611ffc8f4206872aa21fa7ea96a7019df5de90d2
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-13 19:25:08 +0800

    refactoring clean.js in dropdown

--- src/pages/includes/UI-kit/form-elements/dropdown/list/list-button/__clean/clean.js
+++ src/pages/includes/UI-kit/form-elements/dropdown/list/list-button/__clean/clean.js
@@ -1,19 +1,11 @@
-const drop = document.querySelectorAll('.dropdown'); // all dropdown on page
+const btnsClean = document.querySelectorAll('.list-button__clean')
 
-for (let i = 0; i < drop.length; i++) {
-  const btnClean = document.querySelector(`div[data-id="${i + 1}"] .list-button__clean`);
-  const cntrs = document.querySelectorAll(`div[data-id="${i + 1}"] .option-button__counter`); // counters in current dropdown
-  const selectionField = document.querySelector(`div[data-id="${i + 1}"] .dropdown__selection`);
-  console.log(cntrs);
-  btnClean.addEventListener('click', () => {
-    deleteCntrsValue(cntrs);
-    btnClean.classList.remove('list-button__clean_active');
-    selectionField.value = '';
-  });
-}
-
-function deleteCntrsValue(cntrs) {
-  for (cnt of cntrs) {
-    cnt.innerHTML = 0;
-  }
-}
+btnsClean.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.target.offsetParent.querySelectorAll('.option-button__counter').forEach(item => {
+      item.innerHTML = 0
+    })
+    e.target.offsetParent.firstChild.value = ''
+    e.target.classList.remove('list-button__clean_active')
+  })
+})
